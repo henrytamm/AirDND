e8bf6358d9077351a9f85c6a2343d6774b82bb8b
Henry <henrytam95@gmail.com>
2022-11-08 16:44:11 -0800

    some models made

--- backend/db/migrations/20221109003112-create-user.js
+++ backend/db/migrations/20221109003112-create-user.js
@@ -1,26 +1,31 @@
-"use strict";
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("Users", {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      username: {
-        type: Sequelize.STRING(30),
-        allowNull: false,
-        unique: true
+      id: {
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
       },
       email: {
-        type: Sequelize.STRING(256),
-        allowNull: false,
-        unique: true,
+        type: Sequelize.STRING
+      },
+      username: {
+        type: Sequelize.STRING
       },
       hashedPassword: {
-        type: Sequelize.STRING.BINARY,
-        allowNull: false
+        type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
@@ -34,7 +39,7 @@ module.exports = {
       }
     });
   },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable("Users");
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
   }
 };
